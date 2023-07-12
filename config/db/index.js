b27377bba5beabe5df10eb16909b859a3db41155
thanh <vtp5334@gmail.com>
2023-07-12 18:28:40 +0200

    5330

--- config/db/index.js
+++ config/db/index.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 async function connect(){
     try {
         //mongodb+srv://vtp5332:<password>@cluster0.fbqwsig.mongodb.net/
-        await mongoose.connect('mongodb+srv://vtp5332:thanhphan76@cluster0.fbqwsig.mongodb.net/',{
+        //mongodb+srv://vtp5330:<password>@cluster0.6zftsl6.mongodb.net/
+        await mongoose.connect('mongodb+srv://vtp5330:thanhphan76@cluster0.6zftsl6.mongodb.net/',{
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
